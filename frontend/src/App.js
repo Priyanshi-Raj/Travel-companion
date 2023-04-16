dfb68f759781948a30af69383dbcad312d6885c8
Priyanshi-Raj <priyanshiraj2909@gmail.com>
2023-04-17 02:15:17 +0530

    budget planner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Landing from './components/Landing'
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
+import Budget from './components/Budget';
 // import Header from './components/header/Header';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     <>
      <Routes>
         <Route path='/' element={<Landing />} />
-        
+        <Route path='/budget' element={<Budget />} />
       </Routes>
       <Footer/>
 
