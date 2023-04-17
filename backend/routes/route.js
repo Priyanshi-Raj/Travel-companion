75a1c14d98d958c67ace4f85b050da5eecf3ea13
vansh22 <vansh22022002@gmail.com>
2023-04-17 08:32:32 +0530

    local post backend creation

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import { userSignUp, userLogin,ownerLogin,ownerSignUp } from '../controller/user-controller.js';
+import { getPost, createPost } from '../controller/posts.js';
+
 const router= express.Router();
 router.post('/signup/user',userSignUp);
 router.post('/login/user',userLogin);
 router.post('/signup/owner',ownerSignUp);
 router.post('/login/owner',ownerLogin);
+router.post('/localposts', createPost);
 export default router;
