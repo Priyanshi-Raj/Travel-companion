35216f160ba1b3877339b9915e72621219ae735a
Anjali <anjali17162@gmail.com>
2023-04-25 03:04:06 +0530

    added recommend

--- frontend/src/components/RecommendOnQuery.js
+++ frontend/src/components/RecommendOnQuery.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import recommend from "./assets/images/recommend.avif";
+import style from "./RecommendOnQuery.module.css";
+import house3 from "./assets/images/house3.jpg";
+import { useState } from "react";
 
 const RecommendOnQuery = () => {
-  return (
-    <div>RecommendOnQuery</div>
-  )
-}
+  const [text, setText] = useState("");
 
-export default RecommendOnQuery
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(text);
+    setText("");
+  }
+
+  function handleClear() {
+    setText("");
+  }
+
+  return (
+    <div className={style.rec}>
+      <img src={recommend}></img>
+      <h3>Travel Destination Recommendation System</h3>
+      <form className={style.fm} onSubmit={handleSubmit}>
+        <div className={style.ltx}>
+          <label className={style.lx} for="story">
+            Enter text to search
+          </label>
+          <textarea
+            id="story"
+            rows="2"
+            cols="50"
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Write anything...\n"
+            className={style.tx}
+          ></textarea>
+        </div>
+        <br />
+        <div>
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+          <button type="button" onClick={handleClear} className="clear-button">
+            Reset
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default RecommendOnQuery;
